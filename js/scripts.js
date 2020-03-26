ae3854223bc0e4f9ee6dd71f6063f4c7809c30cb
Antonio Cruz <codrocks.cruz3@gmail.com>
2020-03-26 11:41:01 -0700

    add stipulation for males under age 26 will pay extra 50 dollars

--- js/scripts.js
+++ js/scripts.js
@@ -3,10 +3,18 @@ $(document).ready(function() {
     var age = parseInt($("input#age").val());
     var gender = $("select#gender").val();
 
+  if (age) {
+      // if (age === true) 
     var quote = (100 - age) * 3;
-
+    if (gender === 'male' && age < 26) {
+      // quote = quote + 50 //
+      quote += 50; 
+    }
     $("#rate").empty().append(quote);
-    $("#quote").show();
+    $("#quote").show();  
+  } else {
+    alert('You forgot to enter your age.');
+  }
 
     event.preventDefault();
   });
